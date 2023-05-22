b8237e7d40081342ce5c2c98bbba16feec2c734c
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-05-23 00:33:10 +0530

    first commit

---
+++ src/Store/index.js
@@ -0,0 +1,12 @@
+import {configureStore} from '@reduxjs/toolkit'
+import uiSlice from './uiSlice'
+
+
+
+const Store=configureStore({
+    reducer:{
+        ui:uiSlice.reducer
+    }
+})
+
+export default Store
---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,21 @@
+import { uiActions } from '../../Store/uiSlice';
+import classes from './CartButton.module.css';
+import { useDispatch} from 'react-redux'
+
+const CartButton = (props) => {
+
+  const dispatch=useDispatch()
+  
+
+  const toggler=()=>{
+    dispatch(uiActions.toggle())
+  }
+  return (
+    <button className={classes.button} onClick={toggler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
