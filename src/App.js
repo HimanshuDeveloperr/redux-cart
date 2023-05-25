a6fb0cf46e730de705b269b6f9ab1d3cc3705e1a
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-05-26 00:30:35 +0530

    sendingHttpRequestWhileMaintainingReducerProperty

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import Cart from './components/Cart/Cart';
+import { useEffect } from 'react';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useSelector} from 'react-redux'
 function App() {
   
   const showCart=useSelector(state=>state.ui.cartVisibile)
-
+  const Cart=useSelector(state=>state.cart)
+  useEffect(()=>{
+    fetch(`https://redux-cart-16422-default-rtdb.firebaseio.com/cart.json`,{
+      method:'PUT',
+      body:JSON.stringify(Cart)
+    })
+  },[Cart])
   return (
     <Layout>
       {showCart && <Cart />}
